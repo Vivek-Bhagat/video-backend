76ef830ed08e226760b9f407317011f8d2746ada
Vivek-Bhagat <vivekbhagat1890@hotmail.com>
2024-06-06 16:56:08 +0530

    apiresponse

---
+++ src/utils/apiResponse.js
@@ -0,0 +1,8 @@
+class apiResponse {
+    constructor(status, message ="Success", data) {
+        this.status = status;
+        this.message = message;
+        this.data = data;
+        this.sucess= statusCode < 400
+    }
+}
