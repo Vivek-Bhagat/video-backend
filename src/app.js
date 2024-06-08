4eb3cc73b52932c3f29896277d16fb37b34ab719
Vivek-Bhagat <vivekbhagat1890@hotmail.com>
2024-06-08 21:15:17 +0530

    routes and asyncHandler

--- src/app.js
+++ src/app.js
@@ -28,4 +28,11 @@ app.use(express.static("public"));
 
 app.use(cookieParser());
 
+//Routes
+import userRouter from "./routes/user.route.js";
+
+//Routes declerations
+app.use("/api/v1/users", userRouter);
+
+// http://localhost:8080/api/v1/users/register
 export { app };
